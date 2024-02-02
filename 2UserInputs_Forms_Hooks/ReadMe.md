d2c100beebde023bfdfd87f3710628a44ee741f5
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-02-02 12:07:49 +0530

    Add files via upload

---
+++ 2UserInputs_Forms_Hooks/ReadMe.md
@@ -0,0 +1,19 @@
+## Overview
+
+This repository contains a React project that demonstrates how to create and maintain forms, handle user inputs, and how to fetch data from an API to build a basic news website.
+
+## Features
+
+This project includes the following features:
+
+1. Form Creation: The code demonstrates how to create forms using React and how to handle user inputs.
+2. State Management: It showcases how to manage the form data using React's state.
+3. API Integration: It includes code to fetch data from a remote API to display news articles on the website.
+4. Simple and Elegant UI: The project has an elegant and user-friendly UI using React's styling capabilities.
+
+## Prerequisites
+
+Make sure you have the following software installed on your computer:
+
+1. Node.js - [Download and Install Node.js](https://nodejs.org/en/download/)
+2. Git - [Download and Install Git](https://git-scm.com/downloads)
---
+++ 2UserInputs_Forms_Hooks/UserForm.js
@@ -0,0 +1,52 @@
+const { useState } = require("react");
+
+function UserForm(){
+    const [name, setName] = useState("");
+
+    const [fruits, setFruit] = useState("");
+
+    const nameInputRef = useRef(null);
+    const fruitSelectRef = useRef(null);
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const nameInputValue = nameInputRef.current.value;
+        const fruitValue = fruitSelectRef.current.value;
+        alert(nameInputValue + fruitValue);
+    }
+
+    function handleChange(e) {
+        console.log(e.target.value);
+        setFruit(e.target.value);
+    }
+
+    const arr = [1, 2, 3];
+
+    return (
+        <form onSubmit={handleSubmit} style={{ padding: "20px", margin: "20px" }}>
+            <label htmlFor="name">Name</label>
+            {/* <input type="text" onChange={(event) => setName(event.target.value)}/> */}
+            <input type="text" id="nameInput" ref={nameInputRef} />
+            <br />
+            <select ref={fruitSelectRef} onChange={handleChange}>
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+            </select>
+
+            <select name="" id="">
+            {arr.map((element) => {
+                return <option>choose {element}</option>
+            })}
+        </select>
+
+
+            <br />
+
+            <button onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+}
+
+export default UserForm;
